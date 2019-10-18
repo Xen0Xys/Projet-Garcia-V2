6b94273287eb994d4c9c9fad50ddf104519ec820
Xen0Xys <red57101@gmail.com>
2019-10-18 12:44:13 +0200

    End splitByEstablishment function

--- sorting.py
+++ sorting.py
@@ -1,17 +1,21 @@
 class SortingAlgorithm():
     def __init__(self, *_lists):
+        self.to_return = []
         for list in _lists:
-            self.doTraitment(list)
+            res = self.doTraitment(list)
+            if res != "Error":
+                self.to_return.append(res)
     def doTraitment(self, _list):
-        if not self.checkListIntegrity(_list):
-            pass
+        if self.checkListIntegrity(_list) != "Error":
+            return "Error" #Here do splits and traitment
+        else: return "Error"
     def checkListIntegrity(self, _lists):
         for part in _lists:
             try:
                 part["id"]
                 #....
             except KeyError:
-                return None
+                return "Error"
     def splitByExcellencies(self, _list):
         excellencies_list = []
         not_excellencies_list = []
@@ -22,6 +26,7 @@ class SortingAlgorithm():
                 not_excellencies_list.append(part)
         return excellencies_list, not_excellencies_list
     def splitByEstablishment(self, _list):
+        splt_estab_list = {}
         def listEstablishment(_list):
             estab_list = []
             for part in _list:
@@ -29,6 +34,11 @@ class SortingAlgorithm():
                     estab_list.append(part["establishment"])
             return estab_list
         estab_list = listEstablishment(_list)
+        for key in estab_list:
+            splt_estab_list[key] = []
+            for part in _list:
+                splt_estab_list[key].append(part)
+        return splt_estab_list
 
 
 if __name__ == "__main__":
