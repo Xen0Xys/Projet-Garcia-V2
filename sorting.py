e0dedd8703c6f1aa6a473d6aaf554b0c6a5830a6
Xen0Xys <red57101@gmail.com>
2019-10-19 11:08:46 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -7,12 +7,21 @@ class SortingAlgorithm():
                 self.to_return.append(res)
     def doTraitment(self, _list):
         if self.checkListIntegrity(_list) != "Error":
+            exc, n_exc = self.splitByExcellencies(_list)
+            """
+            exc_splt_estab = self.splitByEstablishment(exc)
+            n_exc_splt_estab = self.splitByEstablishment(n_exc)
+            Not sure
+            """
+            exc_splt_cat = self.sortByCategory(exc)
+            n_exc_splt_cat = self.sortByCategory(n_exc)
             return "Error" #Here do splits and traitment
         else: return "Error"
     def checkListIntegrity(self, _lists):
         for part in _lists:
             try:
                 part["id"]
+                part["category"]
                 #....
             except KeyError:
                 return "Error"
@@ -40,7 +49,30 @@ class SortingAlgorithm():
                 splt_estab_list[key].append(part)
         return splt_estab_list
     def sortByCategory(self, _list):
-        categories = {""}
+        categories = {"d_m": [], "d_g": [], "s_f": [], "s_g": [], "unknow": []}
+        for part in _list:
+            if part["category"] == "d_m":
+                categories["d_m"].append(part)
+            elif part["category"] == "d_g":
+                categories["d_g"].append(part)
+            elif part["category"] == "s_f":
+                categories["s_f"].append(part)
+            elif part["category"] == "s_g":
+                categories["s_g"].append(part)
+            else:
+                categories["unknow"].append(part)
+        return categories
+    def sortByClassment(self, _category_list):
+        for category in _category_list.key():
+            if category != "unknow":
+                pass
+                #Do sort
+    def doHen(self, _category_list, _hen_nbre):
+        for category in _category_list.key():
+            if category != "unknow":
+                var_dir = 1
+
+        
 
 
 if __name__ == "__main__":
