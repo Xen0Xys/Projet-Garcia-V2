e0e20c0d5e5bdb706d40e043770153a85061b592
Xen0Xys <red57101@gmail.com>
2019-11-29 12:50:05 +0100

    Update All

--- database.py
+++ database.py
@@ -16,8 +16,8 @@ def deserializeAll(_file_name):
         for j in range(len(all_data)):
             if len(all_data[j]) == 13:
                 if j != 0:
-                    print(all_data[j][i])
-                    t.append(all_data[j][i])
+                    if all_data[j][1] != "":
+                        t.append(all_data[j][i])
         to_return[all_data[0][i]] = t
     return to_return
 
@@ -27,18 +27,34 @@ def insertAllIntoDb(_dico, db):
     for i in range(lenght):
         first_name = _dico["Prénom"][i]
         last_name = _dico["Nom"][i]
-        excellence = _dico["N° équipe"][i]
+        team_number = _dico["N° équipe"][i]
         establishment = _dico["Nom étab."][i]
-        curs.execute("INSERT INTO players VALUES(NULL, '{}', '{}', '{}', '{}')".format(first_name, last_name, excellence, establishment))
+        curs.execute("INSERT INTO players VALUES(NULL, '{}', '{}', '{}', '{}');".format(first_name, last_name, team_number, establishment))
     curs.close()
     db.commit()
 
-os.remove("database.db")
-if not os.path.exists("database.db"):
+def getDataForSort():
     db = sqlite3.connect("database.db")
     curs = db.cursor()
-    curs.execute('''CREATE TABLE "players" ("id" INTEGER NOT NULL UNIQUE, "first_name" VARCHAR(100) NOT NULL, "last_name" VARCHAR(100) NOT NULL, "team_number" VARCHAR(3) NOT NULL, "establishment" VARCHAR(100) NOT NULL, PRIMARY KEY("id" AUTOINCREMENT));''')
-    db.commit()
-    curs.close()
-    insertAllIntoDb(deserializeAll("Feuille_de_match_Etablissement_2010.csv"), db)
+    curs.execute("""SELECT id, first_name, last_name, name_establishment, team_number FROM players;""")
+    res = curs.fetchall()
+    to_return = []
+    for player in res:
+        player_dico = {}
+        player_dico["id"] = int(player[0])
+        player_dico["first_name"] = player[1]
+        player_dico["last_name"] = player[2]
+        player_dico["name_establishment"] = player[3]
+        player_dico["team_number"] = int(player[4])
+        to_return.append(player_dico)
+    return to_return
+
+def initDatabase():
+    if not os.path.exists("database.db"):
+        db = sqlite3.connect("database.db")
+        curs = db.cursor()
+        curs.execute('''CREATE TABLE "players" ("id" INTEGER NOT NULL UNIQUE, "first_name" VARCHAR(100) NOT NULL, "last_name" VARCHAR(100) NOT NULL, "team_number" VARCHAR(3) NOT NULL, "name_establishment" VARCHAR(100) NOT NULL, PRIMARY KEY("id" AUTOINCREMENT));''')
+        db.commit()
+        curs.close()
+        insertAllIntoDb(deserializeAll("Feuille_de_match_Etablissement_2010.csv"), db)
     
--- sorting.py
+++ sorting.py
@@ -1,3 +1,5 @@
+import database
+
 """
 Etapes:
 Rassembler les equipes
@@ -8,19 +10,27 @@ class IntegrityError(Exception):
     """Raised when list's integrity was not guaranted"""
     pass
 
-class SortingAlgorithmV2():
-    def __init__(self, _list, t=""):
+class SortingAlgorithm():
+    def __init__(self, t=""):
         pass
-    def doTraitment(self, _list):
-        print("[INFO] : Starting traitment. (doTraitment function in SortingAlgorithmV2 class)")
+    def doTraitment(self):
+        print("[INFO] : Initializing database. (doTraitment function in SortingAlgorithm class)")
+        database.initDatabase()
+        print("[INFO] : Getting data from database. (doTraitment function in SortingAlgorithm class)")
+        _list = database.getDataForSort()
+        self.checkListIntegrity(_list)
+        print("[INFO] : Starting traitment. (doTraitment function in SortingAlgorithm class)")
         teams =  self.doTeams(_list)
         #teams_nbre = self.countTeams(teams)
         flat_teams_list = self.flattenTeamsList(teams)
         hens_1, hens_2 = self.doHens(flat_teams_list)
-        print("[INFO] : Traitment end, returning values. (doTraitment function in SortingAlgorithmV2 class)")
+        for team in hens_1:
+            for player in team:
+                print(player)
+        print("[INFO] : Traitment end, returning values. (doTraitment function in SortingAlgorithm class)")
     def checkListIntegrity(self, _lists):
-        print("[INFO] : Checking list integrity. (checkListIntegrity function in SortingAlgorithmV2 class)")
-        keys = ["id", "last_name", "first_name", "name_establishment", "team_number", "license_number"]
+        print("[INFO] : Checking list integrity. (checkListIntegrity function in SortingAlgorithm class)")
+        keys = ["id", "last_name", "first_name", "name_establishment", "team_number"]
         for part in _lists:
             try:
                 for key in keys:
@@ -28,32 +38,32 @@ class SortingAlgorithmV2():
                 part_key_nbre = len(part.keys())
                 keys_type_nbre = len(keys)
                 if part_key_nbre != keys_type_nbre:
-                    print("[WARNING] : {} keys give, only {} required. (checkListIntegrity function in SortingAlgorithmV2 class)".format(part_key_nbre, keys_type_nbre))
+                    print("[WARNING] : {} keys give, only {} required. (checkListIntegrity function in SortingAlgorithm class)".format(part_key_nbre, keys_type_nbre))
             except KeyError:
-                print("[ERROR] : KeyError except, raised IntegrityError. (checkListIntegrity function in SortingAlgorithmV2 class)")
+                print("[ERROR] : KeyError except, raised IntegrityError. (checkListIntegrity function in SortingAlgorithm class)")
                 raise IntegrityError
     def doTeams(self, _list):
-        print("[INFO] : Doing Teams. (doTeams function in SortingAlgorithmV2 class)")
+        print("[INFO] : Doing Teams. (doTeams function in SortingAlgorithm class)")
         def listEstablishments(_list):
-            print("[INFO] : Listing establishments. (listEstablishments function in doTraitment function in SortingAlgorithmV2 class)")
+            print("[INFO] : Listing establishments. (listEstablishments function in doTraitment function in SortingAlgorithm class)")
             estab_list = []
             for player in _list:
-                if player[""] not in estab_list: #Definir une fonction avec un seuil d'acceptation
-                    estab_list.append(player[""])
+                if player["name_establishment"] not in estab_list: #Definir une fonction avec un seuil d'acceptation
+                    estab_list.append(player["name_establishment"])
             return estab_list
         def determinMaxTeamsByEstablishment(_list, _estab_list):
-            print("[INFO] : Determining max teams by establishments. (determinMaxTeamsByEstablishment function in doTraitment function in SortingAlgorithmV2 class)")
+            print("[INFO] : Determining max teams by establishments. (determinMaxTeamsByEstablishment function in doTraitment function in SortingAlgorithm class)")
             numbers = {}
             to_return = {}
             for estab in _estab_list:
                 numbers[estab] = [0]
             for player in _list:
                 try:
-                    numbers[player["name_establishment"]][player["team_number"]] += 1
+                    numbers[player["name_establishment"]][(player["team_number"]) - 1] += 1
                 except IndexError:
-                    while len(numbers[player["name_establishment"]]) < [player["team_number"]]:
-                        numbers[player["name_establishment"]].append[0]
-                    numbers[player["name_establishment"]][player["team_number"]] += 1
+                    while len(numbers[player["name_establishment"]]) < player["team_number"]:
+                        numbers[player["name_establishment"]].append(0)
+                    numbers[player["name_establishment"]][(player["team_number"]) - 1] += 1
             for estab_name in numbers.keys():
                 to_return[estab_name] = len(numbers[estab_name])
             return to_return
@@ -62,26 +72,26 @@ class SortingAlgorithmV2():
         estab_nbre = determinMaxTeamsByEstablishment(_list, estab_list)
         for estab_name in estab_nbre.keys(): #setup good numbers of lists in each "teams" indexs
             teams[estab_name] = []
-            for _ in len(estab_nbre[estab_name]):
+            for _ in range(estab_nbre[estab_name]):
                 teams[estab_name].append([])
         for player in _list:
-            teams[player["name_establishment"]][player["team_number"]].append(player)
+            teams[player["name_establishment"]][(player["team_number"]) - 1].append(player)
         return teams
     def countTeams(self, _teams_list):
-        print("[INFO] : Counting team's total number. (countTeams function in SortingAlgorithmV2 class)")
+        print("[INFO] : Counting team's total number. (countTeams function in SortingAlgorithm class)")
         teams_nbre = 0
         for estab_name in _teams_list.keys():
             teams_nbre += len(_teams_list[estab_name])
         return teams_nbre
     def flattenTeamsList(self, _teams_list):
-        print("[INFO] : Flatten teams least. (flattenTeamsList function in SortingAlgorithmV2 class)")
+        print("[INFO] : Flatten teams least. (flattenTeamsList function in SortingAlgorithm class)")
         flat_teams_list = []
         for estab in _teams_list.keys():
             for team in _teams_list[estab]:
                 flat_teams_list.append(team)
         return flat_teams_list
     def doHens(self, _flat_teams_list):
-        print("[INFO] : Doing hens. (doHens function in SortingAlgorithmV2 class)")
+        print("[INFO] : Doing hens. (doHens function in SortingAlgorithm class)")
         hens_1 = []
         hens_2 = []
         for i in range(len(_flat_teams_list)):
@@ -92,89 +102,9 @@ class SortingAlgorithmV2():
         return hens_1, hens_2
     def doHensMatchs(self, _hens):
         pass
+   
 
 
-class SortingAlgorithm():
-    def __init__(self, *_lists):
-        self.to_return = []
-        for list in _lists:
-            res = self.doTraitment(list)
-            if res != "Error":
-                self.to_return.append(res)
-    def doTraitment(self, _list):
-        if self.checkListIntegrity(_list) != "Error":
-            exc, n_exc = self.splitByExcellencies(_list)
-            """
-            exc_splt_estab = self.splitByEstablishment(exc)
-            n_exc_splt_estab = self.splitByEstablishment(n_exc)
-            Not sure
-            """
-            exc_splt_cat = self.sortByCategory(exc)
-            n_exc_splt_cat = self.sortByCategory(n_exc)
-            return "Error" #Here do splits and traitment
-        else: return "Error"
-    def checkListIntegrity(self, _lists):
-        for part in _lists:
-            try:
-                if part["id"] == "":
-                    return "Error"
-                if part["category"] == "":
-                    return "Error"
-                #....
-            except KeyError:
-                return "Error"
-    def splitByExcellencies(self, _list):
-        excellencies_list = []
-        not_excellencies_list = []
-        for part in _list:
-            if part["excellencies"] == True:
-                excellencies_list.append(part)
-            else:
-                not_excellencies_list.append(part)
-        return excellencies_list, not_excellencies_list
-    def splitByEstablishment(self, _list):
-        splt_estab_list = {}
-        def listEstablishment(_list):
-            estab_list = []
-            for part in _list:
-                if part["establishment"] not in estab_list:
-                    estab_list.append(part["establishment"])
-            return estab_list
-        estab_list = listEstablishment(_list)
-        for key in estab_list:
-            splt_estab_list[key] = []
-            for part in _list:
-                splt_estab_list[key].append(part)
-        return splt_estab_list
-    def sortByCategory(self, _list):
-        categories = {"d_m": [], "d_g": [], "s_f": [], "s_g": [], "unknow": []}
-        for part in _list:
-            if part["category"] == "d_m":
-                categories["d_m"].append(part)
-            elif part["category"] == "d_g":
-                categories["d_g"].append(part)
-            elif part["category"] == "s_f":
-                categories["s_f"].append(part)
-            elif part["category"] == "s_g":
-                categories["s_g"].append(part)
-            else:
-                categories["unknow"].append(part)
-        return categories
-    def sortByClassment(self, _category_list):
-        for category in _category_list.key():
-            if category != "unknow":
-                pass
-                #Do sort
-    def doHen(self, _category_list, _hen_nbre):
-        for category in _category_list.key():
-            if category != "unknow":
-                var_dir = 1
-
-        
-
-"""
 if __name__ == "__main__":
-    raise IntegrityError
-    list = ["a", "b"]
-    s_a_object = SortingAlgorithm(list)
-"""
+    sorting_object = SortingAlgorithm()
+    sorting_object.doTraitment()
