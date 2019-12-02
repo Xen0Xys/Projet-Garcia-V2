748d24567bb4f5f022c854169a0618fea969e61a
gunmar06 <49071084+gunmar06@users.noreply.github.com>
2019-12-02 16:53:44 +0100

    Update database.py

--- database.py
+++ database.py
@@ -32,11 +32,13 @@ def insertAllIntoDb(_dico, db):
         curs.execute("INSERT INTO players VALUES(NULL, '{}', '{}', '{}', '{}');".format(first_name, last_name, team_number, establishment))
     curs.close()
     db.commit()
+    db.close()
+
 
 def getDataForSort():
     db = sqlite3.connect("database.db")
     curs = db.cursor()
-    curs.execute("""SELECT id, first_name, last_name, name_establishment, team_number FROM players;""")
+    curs.execute("""SELECT id, first_name, last_name, establishment_name, team_number FROM players;""")
     res = curs.fetchall()
     to_return = []
     for player in res:
@@ -44,17 +46,64 @@ def getDataForSort():
         player_dico["id"] = int(player[0])
         player_dico["first_name"] = player[1]
         player_dico["last_name"] = player[2]
-        player_dico["name_establishment"] = player[3]
+        player_dico["establishment_name"] = player[3]
         player_dico["team_number"] = int(player[4])
         to_return.append(player_dico)
+    curs.close()
+    db.close()
     return to_return
+
+def verificationData(_data_in_css, _data_in_database):
+    db = sqlite3.connect("database.db")
+    curs = db.cursor()
+    first_name = []
+    last_name = []
+    establishment_name = []
+    team_number = []
+    for i in range(len(_data_in_database)):
+        first_name.append(_data_in_database[i]["first_name"])
+        last_name.append(_data_in_database[i]["last_name"])
+        establishment_name.append(_data_in_database[i]["establishment_name"])
+        team_number.append(_data_in_database[i]["team_number"])
+
+    if  first_name == _data_in_css["Prénom"]:
+        print("le doc a les meme donne que la base de donné")
+    else:
+        for i in range (len(first_name)):
+            if not first_name[i] == _data_in_css["Prénom"][i]:
+                curs.close()
+                db.close()
+                uptdateDatabase(_data_in_css["Prénom"][i], _data_in_database[i], "prenom")
+
+
+    if  first_name == _data_in_css["Nom"]:
+        print("le doc a les meme donne que la base de donné")
+    else:
+        for i in range (len(last_name)):
+            if not last_name[i] == _data_in_css["Nom"][i]:
+                curs.close()
+                db.close()
+                uptdateDatabase(_data_in_css["Nom"][i], _data_in_database[i], "nom",)
+
+
+def uptdateDatabase(_data_uptdate, _data_in_database, column):
+    os.remove("database.db")
+    initDatabase()
+
+
 
 def initDatabase():
     if not os.path.exists("database.db"):
         db = sqlite3.connect("database.db")
         curs = db.cursor()
-        curs.execute('''CREATE TABLE "players" ("id" INTEGER NOT NULL UNIQUE, "first_name" VARCHAR(100) NOT NULL, "last_name" VARCHAR(100) NOT NULL, "team_number" VARCHAR(3) NOT NULL, "name_establishment" VARCHAR(100) NOT NULL, PRIMARY KEY("id" AUTOINCREMENT));''')
+        curs.execute('''CREATE TABLE "players" ("id" INTEGER NOT NULL UNIQUE, "first_name" VARCHAR(100) NOT NULL, "last_name" VARCHAR(100) NOT NULL, "team_number" VARCHAR(3) NOT NULL, "establishment_name" VARCHAR(100) NOT NULL, PRIMARY KEY("id" AUTOINCREMENT));''')
         db.commit()
         curs.close()
         insertAllIntoDb(deserializeAll("Feuille_de_match_Etablissement_2010.csv"), db)
+    else:
+        verificationData(deserializeAll("Feuille_de_match_Etablissement_2010.csv"), getDataForSort())
+
+
+
+initDatabase()
     
